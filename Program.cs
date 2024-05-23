b5057d6e7be0208c38e503f671f86ead92099d21
Aiscold <aisjack23@gmail.com>
2024-05-23 14:21:13 +0800

    Add Month Names

--- Program.cs
+++ Program.cs
@@ -5,8 +5,50 @@ int yearProvidedToInt = Int32.Parse(yearProvided);
 int monthsInAYear = 12;
 int daysInAWeek = 7;
 
+
+
 for(int currentMonth = 1; currentMonth <= monthsInAYear; currentMonth++)
 {
+    switch(currentMonth)
+    {
+        case 1:
+        Console.WriteLine("January");
+        break;
+        case 2:
+        Console.WriteLine("February");
+        break;
+        case 3:
+        Console.WriteLine("March");
+        break;
+        case 4:
+        Console.WriteLine("April");
+        break;
+        case 5:
+        Console.WriteLine("May");
+        break;
+        case 6:
+        Console.WriteLine("June");
+        break;
+        case 7:
+        Console.WriteLine("July");
+        break;
+        case 8:
+        Console.WriteLine("August");
+        break;
+        case 9:
+        Console.WriteLine("September");
+        break;
+        case 10:
+        Console.WriteLine("October");
+        break;
+        case 11:
+        Console.WriteLine("November");
+        break;
+        case 12:
+        Console.WriteLine("December");
+        break;
+    }
+
     int numberOfDaysInCurrentMonth = DateTime.DaysInMonth(yearProvidedToInt, currentMonth);
     int i = 0;
 
